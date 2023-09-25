21e0147907adb66366a913086221b8cef308c0d8
Shubhamjindal0801 <shubhamjindal61999@gmail.com>
2023-09-25 21:16:05 +0530

    main

--- src/App.js
+++ src/App.js
@@ -8,46 +8,6 @@ import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 
-  const [search, setSearch] = useState('')
-  const [listening, setListening] = useState(false);
-  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
-
-  useEffect(() => {
-    if (listening) {
-      SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
-    } else {
-      SpeechRecognition.stopListening()
-    }
-    setSearch(transcript)
-  }, [listening])
-
-  useEffect(()=>{
-    document.getElementById('search').focus();
-  },[])
-
-  /*useEffect(()=>{
-    if(search.trim() !== ''){
-      setTimeout(()=>{
-        searchOnGoogle()
-      },1000)
-    }
-  },[search])*/
-
-  if (!browserSupportsSpeechRecognition) {
-    return null
-  }
-
-
-  function searchOnGoogle() {
-    if (search.trim() === "") {
-      alert("Please enter something to search");
-    }
-    else {
-      window.open(`http://google.com/search?q=${search}`);
-    }
-    setSearch("")
-  }
-
   return (
     <div>
       <ToastContainer />
