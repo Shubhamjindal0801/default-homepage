d9421a57e6acdb7fad1269f8a8a5980d1690def6
Shubhamjindal0801 <shubhamjindal61999@gmail.com>
2023-09-25 23:24:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Signup from './components/Signup'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
-import 'react-tooltip/dist/react-tooltip.css'
 
 function App() {
 
